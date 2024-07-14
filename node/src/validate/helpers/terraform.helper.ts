980e0ad706685bd5b210bef3adc8029a63ba3dbe
pepe_fabra_valverde <jofaval@outlook.com>
2024-07-14 13:02:39 +0200

    feat(validate): partial base structure

---
+++ node/src/validate/helpers/terraform.helper.ts
@@ -0,0 +1,7 @@
+export const applyCodeChanges = () => {
+  throw new Error("Not implemented");
+};
+
+export const applyDirectoryChanges = () => {
+  throw new Error("Not implemented");
+};
---
+++ node/src/validate/types/terraform.type.ts
@@ -0,0 +1,3 @@
+export type TerraformCodeChange = {};
+
+export type TerraformDirectoryChange = {};
